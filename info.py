75b00fd33df0de94efd4f201ddafe98173920655
Suhan Park <44104676+suhanpark@users.noreply.github.com>
2022-06-28 16:50:18 -0400

    Delete info.py

--- info.py
+++
@@ -1,5 +0,0 @@
-account_sid = "ACbca5efb53f6ea7bee01b193695ae5a57"
-auth_token = "0a419312beff0a6a7a3d13ca79603376"
-to = "+12489303242"
-from_ = "+19036485449"
-twilio_recovery_code = 'UGczQclqovM8f9_joJnOcr9Ta8Z7-sT-n4sHhnZm'
